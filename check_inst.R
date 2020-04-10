af7d4093ad40b842db4f60e5280c6b924640f3d6
JamieCFreeman <30966718+JamieCFreeman@users.noreply.github.com>
2020-04-10 16:08:17 -0400

    Create check_inst.R

---
+++ check_inst.R
@@ -0,0 +1,21 @@
+#
+# 2020-4-10 JCF
+#
+
+#######################
+# Helper functions
+#######################
+
+#
+# Check package install, return TRUE if exists, FALSE if does not
+#
+
+check_inst <- function(pkg) {
+ 
+   nzchar( system.file(package = "pkg") )
+
+  }
+
+
+# Source:
+# https://stackoverflow.com/questions/9341635/check-for-installed-packages-before-running-install-packages
